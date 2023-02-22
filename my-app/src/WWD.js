1fbe0fed2f6c7b12bd38f749f68945a4472aa4eb
macosta2015 <macosta2015@my.fit.edu>
2023-02-21 20:47:29 -0500

    Update WWD.js

--- my-app/src/WWD.js
+++ my-app/src/WWD.js
@@ -13,7 +13,7 @@ export default function HorizontalCentering() {
         direction="column"
         alignItems="center"
         justify= "center"
-        margin-top= "501"
+        margin-top= "500"
         >
         <Box
           sx={{
